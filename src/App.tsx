6450556093fe2278213723617062abbfe113c60f
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-12-13 13:24:21 -0300

    Projeto Pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { useEffect, useState } from 'react';
-
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
-
+import { Content } from './components/Content';
+import { Header } from './components/Header';
+import { SideBar } from './components/SideBar';
 import { api } from './services/api';
-
-import './styles/global.scss';
-
-import './styles/sidebar.scss';
 import './styles/content.scss';
+import './styles/global.scss';
+import './styles/sidebar.scss';
+
+
+
+
 
 interface GenreResponseProps {
   id: number;
@@ -54,41 +52,17 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
-
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
+      <SideBar 
+        genres={ genres } 
+        handleClick={setSelectedGenreId} 
+        selectedGenreId={selectedGenreId}
+      />
       <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
+        <Header selectedGenre={selectedGenre}/>
 
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+        <Content movies={ movies }/>
       </div>
     </div>
   )
